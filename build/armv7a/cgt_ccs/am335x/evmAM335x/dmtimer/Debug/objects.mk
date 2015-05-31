80ffbcbdcab25794c8878b5743d8502d45a823e4
Jagadish krishna <jagadishkrshn@gmail.com>
2015-05-31 19:40:56 +0530

    Initial commit. Ti's untouched starterware source code

---
+++ build/armv7a/cgt_ccs/am335x/evmAM335x/dmtimer/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := libc.a ../../../../../../../binary/armv7a/cgt_ccs/am335x/drivers/Debug/drivers.lib ../../../../../../../binary/armv7a/cgt_ccs/am335x/system_config/Debug/system.lib ../../../../../../../binary/armv7a/cgt_ccs/am335x/evmAM335x/platform/Debug/platform.lib ../../../../../../../binary/armv7a/cgt_ccs/utils/Debug/utils.lib
+
---
+++ build/armv7a/cgt_ccs/am335x/evmAM335x/dmtimer/Debug/subdir.mk
@@ -0,0 +1,15 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+
+# Each subdirectory must supply rules for building sources it contributes
+dmtimerCounter.null: ../dmtimerCounter.cmd
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM Linker'
+	"../../../../../../../binary/armv7a/cgt_ccs/am335x/evmAM335x/dmtimer/Debug/dmtimerCounter.out" "dmtimerCounter.map" "/lib" "/include"  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
